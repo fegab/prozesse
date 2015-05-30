f5a06450ac69b2c7ab753eb866b5cfec0d775420
fegab <f.gabler@yahoo.de>
2015-05-30 17:06:32 +0200

    f

--- prozesse_HA1_git/src/database/Database.java
+++ prozesse_HA1_git/src/database/Database.java
@@ -77,53 +77,53 @@ public class Database {
 
 	}
 	
-	public int writeMenge(Menge me) throws SQLException {
+//	public int writeMenge(Menge me) throws SQLException {
 
-		Statement stmt = conn.createStatement();
-		String query = "SELECT * FROM menge WHERE index="
-				+ Integer.toString(me.getIdMenge());
-		ResultSet rs = stmt.executeQuery(query);
-
-		if (!rs.next()) {
-
-			String strWriteMenge = "INSERT INTO menge(index INT PRIMARY KEY, uuid VARCHAR(36) NOT NULL, elemente, sense) VALUES(?,?,?,?);";
-
-			PreparedStatement prepStatement = conn
-					.prepareStatement(strWriteMenge);
-
-			// ? füllen
-			//prepStatement.setInt(1, me.getIndex());
-			prepStatement.setString(2, me.getIdMenge().toString());
-			//prepStatement.s(3, me.getElemente());
-			//prepStatement.setDouble(4, bp.getP0().getY());
-
-
-			prepStatement.executeUpdate();
-			return 0; // erstmalig hinzugefügt
-
-		} else {
-
-			String strUpdateMenge;
-			PreparedStatement prepStatement;
-			// keine neue Pavillon-UUID, wenn Bauteil verändert wird
-			if (!me.getPavillonId().toString().equals(strAnfangsUUID)) {
-				strUpdateBodenplatte = "UPDATE bodenplatte SET pavillonUUID= ? WHERE index= ?";
-				prepStatement = conn.prepareStatement(strUpdateBodenplatte);
-				prepStatement.setString(1, bp.getPavillonId().toString());
-				prepStatement.setInt(2, bp.getIndex());
-				prepStatement.executeUpdate();
-			}
-
-			strUpdateBodenplatte = "UPDATE bodenplatte SET x= ? WHERE index= ?";
-			prepStatement = conn.prepareStatement(strUpdateBodenplatte);
-			prepStatement.setDouble(1, bp.getP0().getX());
-			prepStatement.setInt(2, bp.getIndex());
-			prepStatement.executeUpdate();
-
-			return 1; // geupdated
-
-		}
-	}
+//		Statement stmt = conn.createStatement();
+//		String query = "SELECT * FROM menge WHERE index="
+//				+ Integer.toString(me.getIdMenge());
+//		ResultSet rs = stmt.executeQuery(query);
+//
+//		if (!rs.next()) {
+//
+//			String strWriteMenge = "INSERT INTO menge(index INT PRIMARY KEY, uuid VARCHAR(36) NOT NULL, elemente, sense) VALUES(?,?,?,?);";
+//
+//			PreparedStatement prepStatement = conn
+//					.prepareStatement(strWriteMenge);
+//
+//			// ? füllen
+//			//prepStatement.setInt(1, me.getIndex());
+//			prepStatement.setString(2, me.getIdMenge().toString());
+//			//prepStatement.s(3, me.getElemente());
+//			//prepStatement.setDouble(4, bp.getP0().getY());
+//
+//
+//			prepStatement.executeUpdate();
+//			return 0; // erstmalig hinzugefügt
+//
+//		} else {
+//
+//			String strUpdateMenge;
+//			PreparedStatement prepStatement;
+//			// keine neue Pavillon-UUID, wenn Bauteil verändert wird
+//			if (!me.getPavillonId().toString().equals(strAnfangsUUID)) {
+//				strUpdateBodenplatte = "UPDATE bodenplatte SET pavillonUUID= ? WHERE index= ?";
+//				prepStatement = conn.prepareStatement(strUpdateBodenplatte);
+//				prepStatement.setString(1, bp.getPavillonId().toString());
+//				prepStatement.setInt(2, bp.getIndex());
+//				prepStatement.executeUpdate();
+//			}
+//
+//			strUpdateBodenplatte = "UPDATE bodenplatte SET x= ? WHERE index= ?";
+//			prepStatement = conn.prepareStatement(strUpdateBodenplatte);
+//			prepStatement.setDouble(1, bp.getP0().getX());
+//			prepStatement.setInt(2, bp.getIndex());
+//			prepStatement.executeUpdate();
+//
+//			return 1; // geupdated
+//
+//		}
+//	}
 	
 
 }
